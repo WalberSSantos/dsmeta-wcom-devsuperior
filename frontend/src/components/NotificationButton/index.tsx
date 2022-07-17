d7e8b67783d6ce80cad08cc60ac77ebbec07637b
Walber <walber_santos@hotmail.com>
2022-07-17 08:44:04 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import notificationicon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './style.css';
@@ -9,7 +10,7 @@ type Props = {
 
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
-        console.log("SUCESSO")
+        toast.info("SMS enviado com sucesso!")
     })
 }
 
