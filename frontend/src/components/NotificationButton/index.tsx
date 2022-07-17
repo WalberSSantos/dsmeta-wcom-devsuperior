e8c48d75de881b2fd025acbd01dc9fcfd5a9e3e3
Walber <walber_santos@hotmail.com>
2022-07-17 08:35:59 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import notificationicon from '../../assets/img/notification-icon.svg';
-import './style.css'
+import { BASE_URL } from '../../utils/request';
+import './style.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+        console.log("SUCESSO")
+    })
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={notificationicon} alt="Notificação"/>
         </div>
     )
